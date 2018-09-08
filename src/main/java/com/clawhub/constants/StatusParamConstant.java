2b469227d76293df547027738a7c7f4c34fbc442
李志明 <lizhiming@minivision.cn>
2018-09-08 14:22:35 +0800

    常量

---
+++ src/main/java/com/clawhub/constants/StatusParamConstant.java
@@ -0,0 +1,22 @@
+package com.clawhub.constants;
+
+/**
+ * <Description> 状态类常量定义<br>
+ *
+ * @author LiZhiming<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate 2018/9/8 <br>
+ */
+public class StatusParamConstant {
+    /**
+     * 按钮
+     */
+    public static final String RESOURCE_TYPE_BUTTON = "button";
+    /**
+     * 菜单
+     */
+    public static final String RESOURCE_TYPE_MENU = "menu";
+
+
+}
